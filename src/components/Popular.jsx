c0664a75ce7f2d9f7353ed0cf67736f17d480713
선건 <tjsrjs21@gmail.com>
2022-12-22 17:28:17 +0900

    20221222 수정

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -11,12 +11,19 @@ function Popular() {
   }, []);
 
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    );
-    const data = await api.json();
-    console.log(data.recipes);
-    setPopular(data.recipes);
+    const check = localStorage.getItem("popular");
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      console.log(data.recipes);
+      setPopular(data.recipes);
+    }
   };
 
   return (
